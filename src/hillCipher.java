d52b10d98f2367c647187b2eb1123c340a84a773
Brandon Forster <brandonforster@gmail.com>
2012-09-21 15:57:11 -0400

    +: Added code for adding input to char array. Compiles and runs, have not checked for logic errors.

--- src/hillCipher.java
+++ src/hillCipher.java
@@ -8,37 +8,36 @@ import java.io.*;
 
 public class hillCipher {
 
-	/**
-	 * @param args
-	 */
+	private static final int INPUT_ARRAY_SIZE= 10000;
+	private static final int EOF= -1;
+
 	public static void main(String[] args) {
 		Scanner stdin= new Scanner(System.in);
-		
+
 		//Read in filenames. Handle errors later.
 		System.out.println("Please ensure your files are all in this program's directory.\n" +
 				"Please enter the filename of your encryption key.");
-		
+
 		String keyFilename= stdin.next();
 		File keyFile= new File(keyFilename);
-		
+
 		System.out.println("Please enter the filename of your plaintext.");
-		
+
 		String plaintextFilename= stdin.next();
-		File plaintextFile = new File(plaintextFilename);
-		
+
 		System.out.println("Please enter the desired filename of your ciphertext.");
-		
+
 		String outputFilename= stdin.next();
 		File outputFile = new File(outputFilename);
-		
+
 		//Begin reading files. Start with the key. Handle errors here.
 		try {
 			Scanner keyScanner = new Scanner(keyFile);
-			
+
 			//Key matrices are always squares, and here we're reading in
 			//the number of rows and columns.
 			int keyRowsCols= keyScanner.nextInt();
-			
+
 			//double loop that inputs the key matrix into a 2D array that should exactly
 			//match the input.
 			int key[][]= new int[keyRowsCols][keyRowsCols];
@@ -49,12 +48,35 @@ public class hillCipher {
 			}
 		}
 		//Handle bad filename. Just exit program if error found, no recovery.
-		catch (FileNotFoundException fnf)
+		catch (FileNotFoundException e)
 		{
 			System.out.println("Error: bad filename for key. Ending program...");
 			System.exit(1);
 		}
 
-	}
+		//Read in plaintext. Read it char by char, putting everything to lowercase.
+		char plaintext[]= new char[INPUT_ARRAY_SIZE];
+		
+		try {
+			FileReader plaintextStream = new FileReader(plaintextFilename);
+			
+			char currentChar=0;
+			int i= 0;
+			
+			//-1 is very "magic numbery," mostly because it is, but it's the EOF char.
+			while (currentChar != (char) EOF)
+			{
+				currentChar = (char) plaintextStream.read(); // Cast to a char
+				Character.toLowerCase(currentChar); //make it lowercase for consistency
+				plaintext[i]= currentChar; //add it to the massive array
+				i++; //increment the iterator we're using.
+			}
+		}
+		catch (IOException e)
+		{
+			System.out.println("Error: bad filename for plaintext. Ending program...");
+			System.exit(1);
+		}
 
+	}
 }
