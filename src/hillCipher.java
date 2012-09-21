2b2c1a23164c0732d4fac015a8ba61bf9a3ab2fb
Brandon Forster <brandonforster@gmail.com>
2012-09-21 14:14:47 -0400

    +: Input filename code +: Sample run files from assignment

--- src/hillCipher.java
+++ src/hillCipher.java
@@ -3,6 +3,8 @@
 //1 October 2012
 //Assignment 1: Hill Cipher
 
+import java.util.*;
+import java.io.*;
 
 public class hillCipher {
 
@@ -10,7 +12,24 @@ public class hillCipher {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Scanner stdin= new Scanner("System.in");
+		
+		//Read in filenames. Handle errors later.
+		System.out.println("Please ensure your files are all in this program's directory./n" +
+				"Please enter the filename of your encryption key.");
+		
+		File keyFile= new File(stdin.next());
+		
+		System.out.println("Please enter the filename of your plaintext.");
+		
+		File plaintextFile = new File(stdin.next());
+		
+		System.out.println("Please enter the desired filename of your ciphertext.");
+		
+		File outputFile = new File(stdin.next());
+		
+		//Begin reading files. Start with the key. Handle errors here.
+		
 
 	}
 
