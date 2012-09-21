6509167a7dc974475716d87c06f96e3596e28a66
Brandon Forster <brandonforster@gmail.com>
2012-09-21 13:42:13 -0400

    Added the basic class and header comment. No functional code.

---
+++ src/hillCipher.java
@@ -0,0 +1,17 @@
+//Brandon Forster
+//CIS 3360 Fall 2012
+//1 October 2012
+//Assignment 1: Hill Cipher
+
+
+public class hillCipher {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
