230b7724c9a14980b39609fc08c43c663ec5cfd9
Brandon Forster <brandonforster@gmail.com>
2012-09-21 14:31:37 -0400

    +: Key file input is working.

--- src/hillCipher.java
+++ src/hillCipher.java
@@ -12,24 +12,48 @@ public class hillCipher {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		Scanner stdin= new Scanner("System.in");
+		Scanner stdin= new Scanner(System.in);
 		
 		//Read in filenames. Handle errors later.
-		System.out.println("Please ensure your files are all in this program's directory./n" +
+		System.out.println("Please ensure your files are all in this program's directory.\n" +
 				"Please enter the filename of your encryption key.");
 		
-		File keyFile= new File(stdin.next());
+		String keyFilename= stdin.next();
+		File keyFile= new File(keyFilename);
 		
 		System.out.println("Please enter the filename of your plaintext.");
 		
-		File plaintextFile = new File(stdin.next());
+		String plaintextFilename= stdin.next();
+		File plaintextFile = new File(plaintextFilename);
 		
 		System.out.println("Please enter the desired filename of your ciphertext.");
 		
-		File outputFile = new File(stdin.next());
+		String outputFilename= stdin.next();
+		File outputFile = new File(outputFilename);
 		
 		//Begin reading files. Start with the key. Handle errors here.
-		
+		try {
+			Scanner keyScanner = new Scanner(keyFile);
+			
+			//Key matrices are always squares, and here we're reading in
+			//the number of rows and columns.
+			int keyRowsCols= keyScanner.nextInt();
+			
+			//double loop that inputs the key matrix into a 2D array that should exactly
+			//match the input.
+			int key[][]= new int[keyRowsCols][keyRowsCols];
+			for (int i= 0; i < keyRowsCols; i++)
+			{
+				for (int j= 0; j < keyRowsCols; j++)
+					key[i][j]= keyScanner.nextInt();
+			}
+		}
+		//Handle bad filename. Just exit program if error found, no recovery.
+		catch (FileNotFoundException fnf)
+		{
+			System.out.println("Error: bad filename for key. Ending program...");
+			System.exit(1);
+		}
 
 	}
 
