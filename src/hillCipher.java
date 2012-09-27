dfe0486d4504402b7d7f49bb9927444afc416a70
Brandon Forster <brandonforster@gmail.com>
2012-09-26 21:48:19 -0400

    All code functional. Logic error in algorithm.

--- src/hillCipher.java
+++ src/hillCipher.java
@@ -20,32 +20,31 @@ public class hillCipher {
 				"Please enter the filename of your encryption key.");
 
 		String keyFilename= stdin.next();
-		
+
 		if (DEBUG) //just to save myself from typing all this over and over
 			keyFilename= "key";
-		
+
 		File keyFile= new File(keyFilename);
 
 		System.out.println("Please enter the filename of your plaintext.");
 
 		String plaintextFilename= stdin.next();
-		
+
 		if (DEBUG)
 			plaintextFilename= "in";
-		
+
 		File plaintextFile = new File(plaintextFilename);
 
 		System.out.println("Please enter the desired filename of your ciphertext.");
 
 		String outputFilename= stdin.next();
-		
+
 		if (DEBUG)
 			outputFilename= "out";
-		
-		File outputFile = new File(outputFilename);
+
 		int keyRowsCols = 0;
 		int[][] key= new int[500][500];
-		
+
 		//Begin reading files. Start with the key. Handle errors here.
 		try {
 			Scanner keyScanner = new Scanner(keyFile);
@@ -74,31 +73,29 @@ public class hillCipher {
 		try
 		{
 			Scanner plaintextScanner = new Scanner(plaintextFile);
-			
+
 			//read in tokens from the plaintext until we've exhausted it. just concat for now
 			while (plaintextScanner.hasNext() == true)
 				plaintextInput+= plaintextScanner.next();
 		}
 		catch (FileNotFoundException e)
 		{
-			e.printStackTrace();
-			
 			System.out.println("Error: bad filename for plaintext. Ending program...");
 			System.exit(1);
 		}
-		
+
 		//make sure the input is not too long.
 		if (plaintextInput.length() > MAX_INPUT_SIZE)
 		{
 			System.out.println("Error: input too large. Ending program...");
 			System.exit(1);
 		}
-		
+
 		//turn the trimmed and lowered string into a char array. We will now process
 		//this array and put it in a final array.
 		char[] rawPlaintext= plaintextInput.trim().toLowerCase().toCharArray();
 		char[] plaintext= new char[rawPlaintext.length];
-		
+
 		int j= 0;
 		for (int i= 0; i< rawPlaintext.length; i++)
 		{
@@ -108,9 +105,9 @@ public class hillCipher {
 			else
 				plaintext[j++]= rawPlaintext[i];
 		}
-		
-		char[] ciphertext= new char[plaintext.length];
-		
+
+		char[] ciphertext= new char[plaintext.length+1];
+
 		//perform the matrix multiplication. store in the ciphertext array
 		//l= letter		r= row		c= column
 		for (int l= 0; l< plaintext.length; l+= keyRowsCols)
@@ -119,26 +116,41 @@ public class hillCipher {
 			{
 				for (int c=0; c< keyRowsCols; c++)
 				{
-					ciphertext[l+r]+= (char) ((key[r][c]* (int) plaintext[l+c]));
-					System.out.println("key: "+key[r][c]+
-							"\nr: "+r+"    c: "+c+
-							"\nplaintext: " + plaintext[l+c]+
-							"\nciphertext@"+(l+r)+"="+(int)ciphertext[l+r]);
+					if ((l+r) < plaintext.length && (l+c) < plaintext.length)
+						ciphertext[l+r]+= (char) ((key[r][c]* (int) plaintext[l+c]));
+					else
+					{
+						break;
+					}
 				}
+				//mod by 26 and add 'a' to keep it in ascii alphabet
+				//System.out.println("\nciphertext@"+(l+r)+" before mod:\nchar:"+ciphertext[(l+r)]+"     int: "+(int)ciphertext[(l+r)]);
+				ciphertext[(l+r)]= (char) (ciphertext[(l+r)] % 26 + 'a');
+				//System.out.println("\nciphertext@"+(l+r)+" after mod:\nchar:"+ciphertext[(l+r)]+"     int: "+(int)ciphertext[(l+r)]);
 			}
-			//mod by 26 and add 'a' to keep it in ascii alphabet
-			ciphertext[l]= (char) (ciphertext[l] % 26 + 'a');
 		}
-		
-		//print out the finished product, 80 chars per line.
-		for (int i=0; i<ciphertext.length; i++)
+		try
 		{
-			for(j=0; j<LINE_LENGTH; j++)
+			FileWriter fstream = new FileWriter(outputFilename);
+			BufferedWriter out = new BufferedWriter(fstream);
+
+			//print out the finished product, 80 chars per line.
+			for (int i=0; i<ciphertext.length; i++)
 			{
-				if (i<ciphertext.length)
-					System.out.print(ciphertext[i++]);
+				for(j=0; j<LINE_LENGTH; j++)
+				{
+					if (i<ciphertext.length)
+						out.write(ciphertext[i++]);
+				}
+				out.write("\n");
 			}
-			System.out.println();
+			
+			out.close();
+		}
+		catch (IOException e)
+		{
+			System.out.println("Error writing output. Ending program...");
+			System.exit(1);
 		}
 	}
 }
